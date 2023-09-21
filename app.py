8230aff38c900fbac5620ec4ae3b79461a7af684
Quin <quin.marilyn05@gmail.com>
2023-09-20 18:37:28 -0600

    Basic Flask app to download the latest NVDA version (both for modern OS's and for Windows XP).

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect
+from lxml import html
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	r = requests.get("https://www.nvaccess.org/download", headers={
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+	})
+	r.raise_for_status()
+	tree = html.fromstring(r.text)
+	current_version = tree.xpath("//div[@class='download-version__details section-title-v2']//h2")[0].text.replace("NVDA version ", "").strip()
+	url = f"https://www.nvaccess.org/download/nvda/releases/{current_version}/nvda_{current_version}.exe"
+	return redirect(url, code=302)
+
+@app.route("/xp")
+def index_xp():
+	return redirect("https://www.nvaccess.org/download/nvda/releases/2017.3/nvda_2017.3.exe", code=302)
+
+if __name__ == "__main__":
+	app.run()
