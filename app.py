86bb2df944e658f7aa3ade4aa80212515b0ef92a
Quin <quin.marilyn05@gmail.com>
2023-09-21 10:16:13 -0600

    Majorly refactor, but I managed to break stable releases in the process.

--- app.py
+++ app.py
@@ -5,15 +5,29 @@ import re
 
 app = Flask(__name__)
 
+def get_url(type):
+	url = ""
+	r = requests.get(f"https://www.nvaccess.org/nvdaUpdateCheck?versionType={type}")
+	r.raise_for_status()
+	match type:
+		case "snapshot:alpha":
+			match = re.search(r"launcherUrl:\s*(.*)", r.text)
+			if not match:
+				return None
+			url = match.group(1)
+		case "beta", "stable":
+			match = re.search(r"version:\s*(.*)", r.text)
+			if not match:
+				return None
+			version = match.group(1).strip()
+			url = f"https://www.nvaccess.org/download/nvda/releases/{version}/nvda_{version}.exe"
+	return url
+
 @app.route("/")
 def index():
-	r = requests.get("https://www.nvaccess.org/download", headers={
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-	})
-	r.raise_for_status()
-	tree = html.fromstring(r.text)
-	current_version = tree.xpath("//div[@class='download-version__details section-title-v2']//h2")[0].text.replace("NVDA version ", "").strip()
-	url = f"https://www.nvaccess.org/download/nvda/releases/{current_version}/nvda_{current_version}.exe"
+	url = get_url("stable")
+	if not url:
+		return "There was an error getting the latest stable NVDA version"
 	return redirect(url, code=301)
 
 @app.route("/xp")
@@ -22,21 +36,16 @@ def xp():
 
 @app.route("/alpha")
 def alpha():
-	r = requests.get("https://www.nvaccess.org/nvdaUpdateCheck?versionType=snapshot:alpha")
-	match = re.search(r"launcherUrl:\s*(.*)", r.text)
-	if not match:
-		return "There was an error getting the URL for the latest alpha"
-	url = match.group(1)
+	url = get_url("snapshot:alpha")
+	if not url:
+		return "There was an error getting the latest NVDA alpha version"
 	return redirect(url, code=301)
 
 @app.route("/beta")
 def beta():
-	r = requests.get("https://www.nvaccess.org/nvdaUpdateCheck?versionType=beta")
-	match = re.search(r"version:\s*(.*)", r.text)
-	if not match:
-		return "There was an error getting the URL for the latest beta"
-	version = match.group(1).strip()
-	url = f"https://www.nvaccess.org/download/nvda/releases/{version}/nvda_{version}.exe"
+	url = get_url("beta")
+	if not url:
+		return "There was an error getting the latest NVDA beta"
 	return redirect(url, code=301)
 
 @app.errorhandler(404)
