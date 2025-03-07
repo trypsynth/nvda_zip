8b9c0dc8f788167421695528ce44ad3c51cabe65
Quin <TheQuinbox@gmail.com>
2025-03-07 14:43:21 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,6 @@ async fn not_found() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
