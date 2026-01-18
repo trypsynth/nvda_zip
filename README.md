09e016fa11b1ca7acb5d8efd79bd70051db87833
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2026-01-18 13:47:18 +1100

    Add support for launcher hashes (#13)
    * Add hash to nvda_url
    
    * Add Win7 and XP hashes
    
    * Add rustls
    
    * Add hashes to JSON output from nvda_zip
    
    * Lint
    
    * Docs

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ This is a simple Actix-web application that runs at [nvda.zip](https://nvda.zip)
 
 ## Endpoints:
 * `/`: Download the latest stable NVDA version.
-* `/stable.json`: Get a JSON response containing stable NVDA's direct download link.
+* `/stable.json`: Get a JSON response containing stable NVDA's direct download link and hash.
 * `/xp`: Download the last NVDA version to run on Windows XP.
-* `/xp.json`: Get a JSON response containing a direct download link to the last version of NVDA that ran on Windows XP.
+* `/xp.json`: Get a JSON response containing a direct download link to the last version of NVDA that ran on Windows XP and its launcher hash.
 * `/win7`: Download the last NVDA version to run on Windows 7 Service Pack 1 and Windows 8.0.
-* `/win7.json`: Get a JSON response containing a direct download link to the last version of NVDA that ran on Windows 7 Service Pack 1 and Windows 8.0.
+* `/win7.json`: Get a JSON response containing a direct download link to the last version of NVDA that ran on Windows 7 Service Pack 1 and Windows 8.0 and its launcher hash.
 * `/alpha`: Download the latest NVDA snapshot (alpha) version.
-* `/alpha.json`: Get a JSON response containing a direct download link for the latest NVDA snapshot.
+* `/alpha.json`: Get a JSON response containing a direct download link for the latest NVDA snapshot and its launcher hash.
 * `/beta`: Download the latest NVDA beta version.
-* `/beta.json`: Get a JSON response containing the latest NVDA beta's direct download link.
+* `/beta.json`: Get a JSON response containing the latest NVDA beta's direct download link and launcher hash.
 
 ## License
 This project is licensed under the [MIT license](license.md).
--- nvda_url/src/lib.rs
+++ nvda_url/src/lib.rs
@@ -12,23 +12,35 @@ const CACHE_TTL: Duration = Duration::from_secs(30);
 
 /// Direct download link for NVDA 2017.3 (Windows XP).
 pub const XP_URL: &str = "https://download.nvaccess.org/releases/2017.3/nvda_2017.3.exe";
+/// SHA1 hash for NVDA 2017.3 (Windows XP).
+pub const XP_HASH: &str = "386e7acb8cc3ecaabc8005894cf783b51a8ac7f6";
 /// Direct download link for NVDA 2023.3.4 (Windows 7).
 pub const WIN7_URL: &str = "https://download.nvaccess.org/releases/2023.3.4/nvda_2023.3.4.exe";
+/// SHA1 hash for NVDA 2023.3.4 (Windows 7).
+pub const WIN7_HASH: &str = "985a6deab01edb55fbedc9b056956e30120db290";
 
 /// NV Access has their own custom format for NVDA's update API, this lets us parse only the fields we care about out of it.
 #[derive(Debug)]
 struct UpdateInfo {
 	pub launcher_url: Option<String>,
+	/// SHA1 hash of the file pointed to by `launcher_url`.
+	pub launcher_hash: Option<String>,
 }
 
 impl UpdateInfo {
 	#[must_use]
 	fn parse(data: &str) -> Self {
-		let launcher_url = data.lines().find_map(|line| {
-			let (key, value) = line.split_once(": ")?;
-			(key == "launcherUrl").then(|| value.to_owned())
-		});
-		Self { launcher_url }
+		let mut launcher_url = None;
+		let mut launcher_hash = None;
+		for line in data.lines() {
+			let Some((key, value)) = line.split_once(": ") else { continue };
+			match key {
+				"launcherUrl" => launcher_url = Some(value.to_owned()),
+				"launcherHash" => launcher_hash = Some(value.to_owned()),
+				_ => (),
+			}
+		}
+		Self { launcher_url, launcher_hash }
 	}
 }
 
@@ -57,13 +69,14 @@ impl VersionType {
 #[derive(Default)]
 pub struct NvdaUrl {
 	client: Client,
-	cache: Mutex<HashMap<VersionType, (String, Instant)>>,
+	/// url, hash, timestamp
+	cache: Mutex<HashMap<VersionType, (String, String, Instant)>>,
 }
 
 impl NvdaUrl {
 	/// Retrieves the latest download URL for the specified NVDA version type.
 	///
-	/// If a cached URL is still valid, it is returned. Otherwise, a new request is made.
+	/// Thin wrapper around `NvdaUrl::get_details`.
 	///
 	/// # Arguments
 	///
@@ -73,21 +86,37 @@ impl NvdaUrl {
 	///
 	/// An `Option<String>` containing the URL if successful, or `None` if an error occurs.
 	pub async fn get_url(&self, version_type: VersionType) -> Option<String> {
-		let mut cache = self.cache.lock().await;
-		if let Some((url, timestamp)) = cache.get(&version_type)
-			&& timestamp.elapsed() < CACHE_TTL {
-				return Some(url.clone());
-			}
-		let url = self.fetch_url(&version_type).await?;
-		cache.insert(version_type, (url.clone(), Instant::now()));
-		drop(cache);
-		Some(url)
+		Some(self.get_details(version_type).await?.0)
 	}
 
-	async fn fetch_url(&self, version_type: &VersionType) -> Option<String> {
+	/// Retrieves the latest download URL and launcher hash for the specified NVDA version type.
+	///
+	/// If a cached URL is still valid, it is returned. Otherwise, a new request is made.
+	///
+	/// # Arguments
+	///
+	/// * `version_type` - The type of NVDA version to fetch.
+	///  
+	/// # Returns
+	///
+	/// An `Option<(String, String)>` containing the URL and SHA1 hash if successful, or `None` if an error occurs.
+	pub async fn get_details(&self, version_type: VersionType) -> Option<(String, String)> {
+		let mut cache = self.cache.lock().await;
+		if let Some((url, sha1_hash, timestamp)) = cache.get(&version_type)
+			&& timestamp.elapsed() < CACHE_TTL
+		{
+			return Some((url.clone(), sha1_hash.clone()));
+		}
+		let (url, sha1_hash) = self.fetch_url(&version_type).await?;
+		cache.insert(version_type, (url.clone(), sha1_hash.clone(), Instant::now()));
+		drop(cache);
+		Some((url, sha1_hash))
+	}
+
+	async fn fetch_url(&self, version_type: &VersionType) -> Option<(String, String)> {
 		let url = format!("https://api.nvaccess.org/nvdaUpdateCheck?versionType={}", version_type.as_str());
 		let body = self.client.get(&url).send().await.ok()?.text().await.ok()?;
 		let info = UpdateInfo::parse(&body);
-		info.launcher_url
+		Some((info.launcher_url?, info.launcher_hash?))
 	}
 }
